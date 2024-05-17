82491335fec1939c0f9d052987e7259eebe16751
Nympfonic <arys.steam@gmail.com>
2024-05-17 22:02:49 +0100

    Renamed and reorganised solution and project files

---
+++ ExpandedEquipmentSlots/EESPlugin.cs
@@ -0,0 +1,14 @@
+﻿using BepInEx;
+
+namespace Arys.ExpandedEquipmentSlots
+{
+    [BepInPlugin("com.Arys.ExpandedEquipmentSlots", "Arys' Expanded Equipment Slots", "1.0.0")]
+    public class EESPlugin : BaseUnityPlugin
+    {
+        private void Awake()
+        {
+            // Plugin startup logic
+            Logger.LogInfo($"Plugin is loaded!");
+        }
+    }
+}
