6ae067a402a450b68149c17fcef460a574f5666c
Nympfonic <arys.steam@gmail.com>
2024-05-17 22:39:30 +0100

    Added prepatcher

---
+++ ExpandedEquipmentSlotsPatcher/Patcher.cs
@@ -0,0 +1,22 @@
+﻿using Mono.Cecil;
+using System.Collections.Generic;
+
+namespace Arys.ExpandedEquipmentSlotsPatcher
+{
+    public static class Patcher
+    {
+        public static IEnumerable<string> TargetDLLs { get; } = new[] { "Assembly-CSharp.dll" };
+
+        public static void Patch(ref AssemblyDefinition assembly)
+        {
+            AddEquipmentSlots(ref assembly);
+        }
+
+        private static void AddEquipmentSlots(ref AssemblyDefinition assembly)
+        {
+            TypeDefinition equipmentSlot = assembly.MainModule.GetType("EFT.InventoryLogic.EquipmentSlot");
+
+            PatcherHelper.AddEnumValue(ref equipmentSlot, "TacticalBelt");
+        }
+    }
+}
---
+++ ExpandedEquipmentSlotsPatcher/PatcherHelper.cs
@@ -0,0 +1,50 @@
+﻿using Mono.Cecil;
+
+namespace Arys.ExpandedEquipmentSlotsPatcher
+{
+    internal static class PatcherHelper
+    {
+        private const FieldAttributes defaultFieldAttributes
+            = FieldAttributes.Public | FieldAttributes.Static | FieldAttributes.Literal | FieldAttributes.HasDefault;
+
+        internal static void AddEnumValues(ref TypeDefinition typeDef, params string[] names)
+        {
+            foreach (var name in names)
+            {
+                AddEnumValue(ref typeDef, name);
+            }
+        }
+
+        internal static void AddEnumValue(ref TypeDefinition typeDef, string name)
+        {
+            var enumFields = typeDef.Fields;
+
+            for (int i = 0; i < enumFields.Count; i++)
+            {
+                int currentValue = (int)enumFields[i].Constant;
+
+                if (i + 1 == enumFields.Count)
+                {
+                    enumFields.Add(new FieldDefinition(name, defaultFieldAttributes, typeDef)
+                    {
+                        Constant = currentValue + 1
+                    });
+
+                    break;
+                }
+
+                int nextValue = (int)enumFields[i + 1].Constant;
+
+                if (nextValue - currentValue > 1)
+                {
+                    enumFields.Add(new FieldDefinition(name, defaultFieldAttributes, typeDef)
+                    {
+                        Constant = currentValue + 1
+                    });
+
+                    break;
+                }
+            }
+        }
+    }
+}
